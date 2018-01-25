604ea4eaf8626596b40a27775c1376c07b16ca28
vegart13 <35805073+vegart13@users.noreply.github.com>
2018-01-25 15:40:00 +0100

    Vegard Trydal Endring

--- Andreas.go
+++ Andreas.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Andreas er mitt navn!!! Xohan heter jeg!")
+	fmt.Println("Andreas er mitt navn!!! Xohan heter jeg!Vegard Trydal har størst biceps")
 }
