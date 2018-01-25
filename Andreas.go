5260d7bf92c5826db5dede676d211ebe4358e793
vegart13 <35805073+vegart13@users.noreply.github.com>
2018-01-25 15:34:54 +0100

    Vegard Trydal endring
    la til navn

--- Andreas.go
+++ Andreas.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Andreas er mitt navn!!! Xohan heter jeg!")
+	fmt.Println("Andreas er mitt navn!!! Xohan heter jeg! Trydal er sterkest på gruppa")
 }
