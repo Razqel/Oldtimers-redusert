ccec8fa6f1ee03912d871e323b3cd7b554750afb
Yaguel <35783118+Yaguel@users.noreply.github.com>
2018-03-09 16:40:24 +0100

    Delete Andreas.go

--- Andreas.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-
-	fmt.Println("Andreas er mitt navn!!! Xohan heter jeg. Jeg er Vegard Marvik! Vegard Trydal er den tøffeste Vegarden. Yaguel is in!!")
-
-}
